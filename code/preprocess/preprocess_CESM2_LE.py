c3076dfdae69733051350e3e34123ace6243577b
Luna Bloin-Wibe <luna.bloinwibe@env.ethz.ch>
2025-01-20 11:03:38 +0100

    fixed bug in LE30

--- code/preprocess/preprocess_CESM2_LE.py
+++ code/preprocess/preprocess_CESM2_LE.py
@@ -8,6 +8,7 @@ import preproc as pc
 ## paths
 # source data folder
 LE100_path = '/net/meso/climphys/fischeer/CESM-ETH/CESM2-LE/'
+LE30_path = "/net/meso/climphys/cesm212/"
 # processed data
 output_path = '/net/xenon/climphys/lbloin/boost_proba/'
 
@@ -33,12 +34,12 @@ print("preprocessing 30-member LE")
 #selecting all existing PI runs
 pnw_le30 = []
 for mem in tqdm(range(1,31)): #TODO: what about extra members 31-35?
-    file = sorted(glob.glob(in_path+f"b.e212.B*cmip6.f09_g17.001.2005.ens{mem:03d}/archive/atm/hist/b.e212.B*cmip6.f09_g17.001.2005.ens{mem:03d}.cam.h1.*-01-01-00000.nc"))
+    file = sorted(glob.glob(LE30_path+f"b.e212.B*cmip6.f09_g17.001.2005.ens{mem:03d}/archive/atm/hist/b.e212.B*cmip6.f09_g17.001.2005.ens{mem:03d}.cam.h1.*-01-01-00000.nc"))
     with xr.open_mfdataset(file,preprocess = preproc) as ds:
         pnw_le30.append(ds.sel(time=slice("2005","2035"))) # there is a file containing only the first day of january 2036
-    pnw_le30 = xr.concat(pnw_le30,dim = "member")
-    pnw_le30["member"] = range(1,31)
-    pnw_le30 = pnw_le30.set_coords('member')
-    print("opened")
-    pnw_le30.to_netcdf(+f"{output_path}PNW_LE_30_member.nc")
-    print("saved PNW files")
+pnw_le30 = xr.concat(pnw_le30,dim = "member")
+pnw_le30["member"] = range(1,31)
+pnw_le30 = pnw_le30.set_coords('member')
+print("opened")
+pnw_le30.to_netcdf(f"{output_path}PNW_LE_30_member.nc")
+print("saved PNW files")
